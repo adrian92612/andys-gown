72d882e3fc27a79fceae6a98761fc3c79701db3b
adrian92612 <adrianvillamin0612@gmail.com>
2025-04-28 01:13:06 +0800

    Add nuqs useQueryState to sync state with url

--- src/components/app/collections/CollectionsGownGrid.tsx
+++ src/components/app/collections/CollectionsGownGrid.tsx
@@ -3,14 +3,15 @@
 import { Category, Prisma } from "@prisma/client";
 import { GownCard } from "./GownCard";
 import { Input } from "@/components/ui/input";
-import { useMemo, useState } from "react";
 import { CategoryBtn } from "./CategoryBtn";
 import { Button } from "@/components/ui/button";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { parseAsInteger, parseAsStringEnum, useQueryState } from "nuqs";
 
 const PAGE_SIZE = 12;
 
-export type CategoryWithAll = Category | "All";
+const categoryEnums = ["All", ...Object.values(Category)] as const;
+type CategoryWithAll = Category | "All";
 
 type Props = {
   gownList: Prisma.GownGetPayload<{
@@ -19,42 +20,44 @@ type Props = {
 };
 
 export const CollectionsGownGrid = ({ gownList }: Props) => {
-  const [filter, setFilter] = useState("");
-  const [category, setCategory] = useState<CategoryWithAll>("All");
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const categoryList = useMemo(
-    () => gownList.filter((g) => category === "All" || g.category === category),
-    [gownList, category]
+  const [category, setCategory] = useQueryState(
+    "category",
+    parseAsStringEnum([...categoryEnums]).withDefault("All")
+  );
+  const [search, setSearch] = useQueryState("q", {
+    defaultValue: "",
+  });
+  const [currentPage, setCurrentPage] = useQueryState(
+    "page",
+    parseAsInteger.withDefault(1)
   );
 
-  const filteredList = useMemo(
-    () =>
-      categoryList.filter((g) => {
-        const name = g.name.toLowerCase().includes(filter.toLowerCase());
-        const price = g.price.toString().includes(filter);
-        const color = g.color.toLowerCase().includes(filter.toLowerCase());
-        return name || price || color;
-      }),
-    [filter, categoryList]
+  const categoryList = gownList.filter(
+    (g) => category === "All" || g.category === category
   );
+
+  const filteredList = categoryList.filter((g) => {
+    const name = g.name.toLowerCase().includes(search.toLowerCase());
+    const price = g.price.toString().includes(search);
+    const color = g.color.toLowerCase().includes(search.toLowerCase());
+    return name || price || color;
+  });
+
   const maxPage = Math.ceil(filteredList.length / PAGE_SIZE);
 
-  const slicedList = useMemo(
-    () =>
-      filteredList.slice(
-        (currentPage - 1) * PAGE_SIZE,
-        currentPage * PAGE_SIZE
-      ),
-    [filteredList, currentPage]
+  const slicedList = filteredList.slice(
+    (currentPage - 1) * PAGE_SIZE,
+    currentPage * PAGE_SIZE
   );
 
   const handleFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFilter(e.target.value);
+    setSearch(e.target.value);
+    setCurrentPage(1);
   };
 
   const handleSetCategory = (value: CategoryWithAll) => {
     setCategory(value);
+    setCurrentPage(1);
   };
 
   const handlePrevPage = () => {
@@ -68,12 +71,7 @@ export const CollectionsGownGrid = ({ gownList }: Props) => {
   return (
     <section className="bg-site-text text-site-text-light mt-[2px] py-10 px-4 sm:px-8 space-y-10 grow">
       <div className="flex items-center justify-center flex-wrap">
-        <CategoryBtn
-          value="All"
-          category={category}
-          handleClick={handleSetCategory}
-        />
-        {Object.values(Category).map((c, i) => (
+        {categoryEnums.map((c, i) => (
           <CategoryBtn
             key={i}
             value={c}
@@ -86,7 +84,7 @@ export const CollectionsGownGrid = ({ gownList }: Props) => {
       <Input
         placeholder="Search for..."
         className="border-site-background rounded-none text-site-text max-w-80 bg-site-background"
-        value={filter}
+        value={search}
         onChange={handleFilter}
       />
 
